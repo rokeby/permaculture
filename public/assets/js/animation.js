d54442449f03c30081a1831c8c98d64d07a42e0a
agnescameron <agnesfcameron@gmail.com>
2019-08-08 21:13:24 +0300

    added main animation loop (hours mins seconds)

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -2,22 +2,53 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function goatEvent() {
+	console.log('GOOOAAAATS')
+}
 
+function reset() {
+	console.log('gd morning friends')
+}
 
+function eachHour() {
+	console.log('an hour!!')
+}
+
+function eachMinute() {
+	console.log('a minute!!')
+}
+
+function eachSecond() {
+	$('.water').each( function() {
+		var wave = $( this ).html() === "~" ? '≈' : "~"
+		$( this ).html(wave);
+	})	
+}
+
+//queries the time every second, runs regular events
 async function runMainLoop(){
-	console.log('running!')
-	while(true){
 
-		$('.water').each( function() {
-			var wave = $( this ).html() === "~" ? '≈' : "~"
-			$( this ).html(wave);
-		})
+	while(true){
+		var today = new Date();
+
+		if(today.getSeconds() === 0){
+			eachMinute();
+		}
+
+		if(today.getMinutes() === 0){
+			eachHour();
+		}
+
+		if(today.getHours() === 0){
+			reset();
+		}
+
+		eachSecond();
 		await sleep(1000);
 
 	}
-
 }
 
 
 
-export default {runMainLoop};
+export default {goatEvent, runMainLoop};
--- public/assets/js/grid.js
+++ public/assets/js/grid.js
@@ -167,3 +167,4 @@ function generateGrid(){
 
 generateGrid();
 animation.runMainLoop();
+animation.goatEvent();
